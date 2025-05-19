c5147123b3228788712d374ede3f97e77434a1ca
jjmm-del <jimin8768@naver.com>
2025-05-19 19:11:19 +0900

    add: resources
    13강 자원채취 추가

--- 3D Survival/Assets/Scripts/Item/Resource.cs
+++ 3D Survival/Assets/Scripts/Item/Resource.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ItemData itemToGive;
+    public int quantityPerHit = 1;
+    public int capacy;
+    public void Gather(Vector3 hitPoint, Vector3 hitNormal)
     {
-        
+        for (int i = 0; i < quantityPerHit; i++)
+        {
+            if (capacy <= 0) break;
+            capacy -= 1;
+            Instantiate(itemToGive.dropPrefab, hitPoint + Vector3.up,Quaternion.LookRotation(hitNormal, Vector3.up));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
