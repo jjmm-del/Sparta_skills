65125f5aab6db45bdc2cde9e00ef74d7dc202b93
jjmm-del <jimin8768@naver.com>
2025-05-19 17:53:26 +0900

    add: equip, attack
    도끼, 칼 장착 및 애니메이션 추가
    도끼 칼 휘둘러 공격, 자원채집 추가

--- 3D Survival/Assets/Scripts/UI/UIInventory.cs
+++ 3D Survival/Assets/Scripts/UI/UIInventory.cs
@@ -25,6 +25,9 @@ public class UIInventory : MonoBehaviour
 
     ItemData selectedItem;
     int selectedItemIndex = 0;
+
+    int curEquipIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -228,6 +231,35 @@ public class UIInventory : MonoBehaviour
         UpdateUI();
     }
 
+    public void OnEquipButton()
+    {
+        if (slots[curEquipIndex].equipped)
+        {
+            UnEquip(curEquipIndex);
+        }
+        slots[selectedItemIndex].equipped = true;
+        curEquipIndex = selectedItemIndex;
+        CharacterManager.Instance.Player.equip.EquipNew(selectedItem);
+        UpdateUI();
 
+        SelectItem(selectedItemIndex);
+    }
+    void UnEquip(int index)
+    {
+        slots[index].equipped = false;
+        CharacterManager.Instance.Player.equip.UnEquip();
+        UpdateUI();
+
+        if (selectedItemIndex == index)
+        {
+            SelectItem(selectedItemIndex);
+        }
+
+    }
+
+    public void OnUnEquipButton()
+    {
+        UnEquip(selectedItemIndex);
+    }
     
 }
