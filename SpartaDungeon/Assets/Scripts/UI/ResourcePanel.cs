57255da05bb97e31caa9acbcc1bdaaae2bc1b84f
jjmm-del <jimin8768@naver.com>
2025-05-22 21:00:04 +0900

    feat : resourceItem 👍
    리소스 아이템 로직은 일단 전부 구현

--- SpartaDungeon/Assets/Scripts/UI/ResourcePanel.cs
+++ SpartaDungeon/Assets/Scripts/UI/ResourcePanel.cs
@@ -8,7 +8,8 @@ public class ResourcePanel : MonoBehaviour
     
     public TextMeshProUGUI coinText, starText, lifeText;
     public int coinValue, starValue, lifeValue;
-    
+
+    private const int CoinsForExtraLife = 100;
     private void Awake()
     {
         if(Instance == null) Instance = this;
@@ -22,7 +23,25 @@ public class ResourcePanel : MonoBehaviour
         lifeText.text = $"X {lifeValue}";
     }
 
-    public void AddCoin(int amount = 1) => coinValue += amount;
-    public void AddStar(int amount = 1) => starValue += amount;
-    public void AddLife(int amount = 1) => lifeValue += amount;
+    public void AddCoin(int amount = 1)
+    {
+        coinValue += amount;
+        CharacterManager.Instance.Player.condition.Heal(1);
+
+        if (coinValue >= CoinsForExtraLife)
+        {
+            coinValue -= CoinsForExtraLife;
+            AddLife(1);
+        }
+    }
+
+    public void AddStar(int amount = 1)
+    {
+        starValue += amount;   
+    }
+
+    public void AddLife(int amount = 1)
+    {
+        lifeValue += amount;
+    }
 }
